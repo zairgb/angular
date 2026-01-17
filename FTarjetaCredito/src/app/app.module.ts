947918d37546619230c28e9c3f1a606cd1d62766
zairjmg <jair.jmgu@gmail.com>
2026-01-17 17:21:08 -0500

    primer proyecto con tailwindcss

--- FTarjetaCredito/src/app/app.module.ts
+++ FTarjetaCredito/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './Component/header/header.component';
+import { FooterComponent } from './Component/footer/footer.component';
+import { BodyComponent } from './Component/body/body.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    BodyComponent
   ],
   imports: [
     BrowserModule
